e8da77cfb66e52a114542ead337b6321ac19ef13
arrismo <mikaelmoise00@gmail.com>
2024-11-08 15:46:39 -0500

    commiting to move to another folder

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+#from airflow import DAG
+#from airflow.operators.python import PythonOperator
 
 
 default_args = {
@@ -48,21 +48,27 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
     res = get_data()
     res = format_data(res)
-    print(json.dumps(res, indent = 3))
+    # print(json.dumps(res, indent = 3))
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+
+    producer.send('users_created', json.dumps(res).encode('utf-8'))
 
 
 
-with DAG('user-automation',
-         default_args = default_args,
-         schedule='@daily',
-         catchup = False) as dag:
+# with DAG('user-automation',
+#          default_args = default_args,
+#          schedule='@daily',
+#          catchup = False) as dag:
+#
+#     streaming_task = PythonOperator(
+#         task_id = 'stream_data_from_api',
+#         python_callable=stream_data
+#     )
 
-    streaming_task = PythonOperator(
-        task_id = 'stream_data_from_api',
-        python_callable=stream_data
-    )
 
-stream_data()
-#get_data()
+
+#stream_data()
