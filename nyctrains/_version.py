c6a44d226246c34f9e5ee61e24f78145fb033fa7
arrismo <mikaelmoise00@gmail.com>
2025-04-25 10:27:32 -0400

    uploaded version

--- nyctrains/_version.py
+++ nyctrains/_version.py
@@ -17,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '1.5.1.dev0+g99ce9a9.d20250425'
-__version_tuple__ = version_tuple = (1, 5, 1, 'dev0', 'g99ce9a9.d20250425')
+__version__ = version = '1.5.1.dev0+g2bb7fea.d20250425'
+__version_tuple__ = version_tuple = (1, 5, 1, 'dev0', 'g2bb7fea.d20250425')
